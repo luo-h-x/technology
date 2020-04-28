185862fcba8bd8007b1cc759e91c9a72ab9d7255
lhx <lhx@git.com>
2020-04-28 21:34:45 +0800

    slideCaption.js

--- scripts/slideCaption.js
+++
@@ -1,9 +0,0 @@
-!function () {
-  let slidEl = document.querySelector('.slide-caption')
-  let arrSlid = [...slidEl.children]
-  console.log(arrSlid);
-  
-  // arrSlid.map( (v, i) => {
-
-  // })
-}.call()
