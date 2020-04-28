7fd2367b7080fe3fbc9731b2010eaa08297a82fe
lhx <lhx@git.com>
2020-04-28 17:10:50 +0800

    标题transform

---
+++ scripts/slideCaption.js
@@ -0,0 +1,9 @@
+!function () {
+  let slidEl = document.querySelector('.slide-caption')
+  let arrSlid = [...slidEl.children]
+  console.log(arrSlid);
+  
+  // arrSlid.map( (v, i) => {
+
+  // })
+}.call()
