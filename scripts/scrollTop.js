0f958fd18b04666a5acabd7493f7acbac101931b
lhx <lhx@git.com>
2020-05-01 11:49:02 +0800

    scrollTop.js

--- scripts/scrollTop.js
+++ scripts/scrollTop.js
@@ -1,12 +1,10 @@
 !function () {
   let toTopEl = document.getElementsByClassName('scrollToTop')[0]
   let homeEl = document.getElementById('home')
-  console.log(toTopEl,homeEl);
-  
-  window.addEventListener('scroll', () => {
 
+  window.addEventListener('scroll', () => {
     let bottom = homeEl.getBoundingClientRect().bottom
-    if (bottom <0 ) {
+    if (bottom < 0) {
       toTopEl.children[0].style = 'visibility: visible'
     } else {
       toTopEl.children[0].style = 'display: none'
