f6a167071422e57ea76ba9a48f3a84f6888f8af7
lhx <lhx@git.com>
2020-05-01 11:47:10 +0800

    fromHeader.js

--- scripts/fromHeader.js
+++ scripts/fromHeader.js
@@ -9,17 +9,21 @@
   let sections = document.querySelectorAll('section')
   let homeEl = document.getElementById('home')
   let scrollArr = [homeEl, ...sections]
-  
 
   let status = false
+  let hold = true
 
-  activeEl.map((v,i) => {
+  activeEl.map((v, i) => {
     v.addEventListener('click', () => {
       activeEl.map((v1) => {
         v1.classList.remove('active')
       })
+      hold = false
       v.classList.add('active')
-      scrollArr[i].scrollIntoView({behavior: 'smooth', block: 'start'})
+      scrollArr[i].scrollIntoView({ behavior: 'smooth', block: 'start' })
+      let timer = setTimeout(() => {
+        hold = true
+      }, 500)
     })
   })
 
@@ -36,31 +40,31 @@
       headerEl.classList.add('sticky')
       status = true
     }
-    if (aboutTop <= 72) {
+    if (aboutTop <= 72 && hold) {
       activeEl.map((v1) => {
         v1.classList.remove('active')
       })
       activeEl[1].classList.add('active')
     }
-    if (showcaseTop <= 72) {
+    if (showcaseTop <= 72 && hold) {
       activeEl.map((v1) => {
         v1.classList.remove('active')
       })
       activeEl[2].classList.add('active')
     }
-    if (teamTop <= 72) {
+    if (teamTop <= 72 && hold) {
       activeEl.map((v1) => {
         v1.classList.remove('active')
       })
       activeEl[3].classList.add('active')
     }
-    if (dataTop <= 72) {
+    if (dataTop <= 72 && hold) {
       activeEl.map((v1) => {
         v1.classList.remove('active')
       })
       activeEl[4].classList.add('active')
     }
-    if (aboutTop > 72 && status) {
+    if (aboutTop > 200 && status) {
       headerEl.classList.remove('sticky')
       activeEl.map((v1) => {
         v1.classList.remove('active')
