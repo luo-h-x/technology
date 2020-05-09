620027e167d22eda4370b18c059e112d0134fab1
luo-h-x <1877430627@qq.com>
2020-05-09 11:53:18 +0800

    fromheader.js

--- scripts/fromHeader.js
+++ scripts/fromHeader.js
@@ -1,80 +1,78 @@
 !function () {
-  let aboutEl = document.querySelector('#about')
-  let headerEl = document.querySelector('header')
-  let navEl = document.querySelector('.nav')
-  let activeEl = [...navEl.children]
-  let showcaseEl = document.querySelector('#showcase')
-  let teamEl = document.querySelector('#team')
-  let dataEl = document.querySelector('#data')
-  let sections = document.querySelectorAll('section')
-  let homeEl = document.getElementById('home')
-  let scrollArr = [homeEl, ...sections]
+  let aboutEl = document.querySelector("#about");
+  let headerEl = document.querySelector("header");
+  let navEl = document.querySelector(".nav");
+  let activeEl = [...navEl.children];
+  let showcaseEl = document.querySelector("#showcase");
+  let teamEl = document.querySelector("#team");
+  let dataEl = document.querySelector("#data");
+  let sections = document.querySelectorAll("section");
+  let homeEl = document.getElementById("home");
+  let scrollArr = [homeEl, ...sections];
 
-  let status = false
-  let hold = true
+  let status = false;
+  let hold = true;
 
   activeEl.map((v, i) => {
-    v.addEventListener('click', () => {
+    v.addEventListener("click", (e) => {
+      hold = false;
       activeEl.map((v1) => {
-        v1.classList.remove('active')
-      })
-      hold = false
-      v.classList.add('active')
-      scrollArr[i].scrollIntoView({ behavior: 'smooth', block: 'start' })
-      let timer = setTimeout(() => {
-        hold = true
-      }, 500)
-    })
-  })
+        v1.classList.remove("active");
+      });
+      scrollArr[i].scrollIntoView({ behavior: "smooth", block: "start" });
 
-  window.addEventListener('scroll', function () {
-    let teamTop = teamEl.getBoundingClientRect().top
+      v.classList.add("active");
+      setTimeout(() => {
+        hold = true;
+      }, 1000);
+    });
+  });
 
-    let showcaseTop = showcaseEl.getBoundingClientRect().top
+  document.addEventListener("scroll", function (e) {
+    
+    let teamTop = teamEl.getBoundingClientRect().top;
 
-    let aboutTop = aboutEl.getBoundingClientRect().top
+    let showcaseTop = showcaseEl.getBoundingClientRect().top;
 
-    let dataTop = dataEl.getBoundingClientRect().top
+    let aboutTop = aboutEl.getBoundingClientRect().top;
+
+    let dataTop = dataEl.getBoundingClientRect().top;
 
     if (aboutTop <= 0 && !status) {
-      headerEl.classList.add('sticky')
-      status = true
+      headerEl.classList.add("sticky");
+      status = true;
     }
     if (aboutTop <= 72 && hold) {
-      e.stopPropagation()
       activeEl.map((v1) => {
-        v1.classList.remove('active')
-      })
-      activeEl[1].classList.add('active')
+        v1.classList.remove("active");
+      });
+      activeEl[1].classList.add("active");
     }
     if (showcaseTop <= 72 && hold) {
-      e.stopPropagation()
       activeEl.map((v1) => {
-        v1.classList.remove('active')
-      })
-      activeEl[2].classList.add('active')
+        v1.classList.remove("active");
+      });
+      activeEl[2].classList.add("active");
     }
     if (teamTop <= 72 && hold) {
-      e.stopPropagation()
       activeEl.map((v1) => {
-        v1.classList.remove('active')
-      })
-      activeEl[3].classList.add('active')
+        v1.classList.remove("active");
+      });
+      activeEl[3].classList.add("active");
     }
     if (dataTop <= 72 && hold) {
-      e.stopPropagation()
       activeEl.map((v1) => {
-        v1.classList.remove('active')
-      })
-      activeEl[4].classList.add('active')
+        v1.classList.remove("active");
+      });
+      activeEl[4].classList.add("active");
     }
     if (aboutTop > 200 && status) {
-      headerEl.classList.remove('sticky')
+      headerEl.classList.remove("sticky");
       activeEl.map((v1) => {
-        v1.classList.remove('active')
-      })
-      activeEl[0].classList.add('active')
-      status = false
+        v1.classList.remove("active");
+      });
+      activeEl[0].classList.add("active");
+      status = false;
     }
-  })
-}.call()
+  });
+}.call();
